08e831309d34c188103ab92349d676ccb90275a0
Mykhailo <mikhail.garasimchuk@dataart.com>
2023-09-14 07:33:00 +0200

    Remove

--- Concord.WebPortal.WebApi/Infrastructure/AuthorizationPolicies.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Concord.WebPortal.WebApi.Infrastructure
-{
-    public class AuthorizationPolicies
-    {
-        public const string AssignmentToSaleTypeMappingReadRoleRequired = "AssignmentToSaleTypeMappingReadRoleRequired";
-        public const string AssignmentToSaleTypeMappingReadWriteRoleRequired = "AssignmentToSaleTypeMappingReadWriteRoleRequired";
-    }
-}
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./Concord.WebPortal.WebApi.exe
